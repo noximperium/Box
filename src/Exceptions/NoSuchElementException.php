4bf819cbd56ef4cbcc48bd0ab0571a14396df5d2
Kaka <kakalition@gmail.com>
2022-10-13 13:14:30 +0700

    Remove unused files

--- src/Exceptions/NoSuchElementException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class NoSuchElementException extends Exception
-{
-  public function __construct($message, $code = 0, Throwable $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
-}
