4cfbe02554e75edde06817239d74553d646a884b
Kaka <kakalition@gmail.com>
2022-10-17 14:21:37 +0700

    remove unused files

--- src/Box.php
+++ src/Box.php
@@ -7,12 +7,12 @@ use NoxImperium\Container\MutableList;
 
 class Box
 {
-  public static function createImmutableList($val)
+  public static function immutableList($val)
   {
     return new ImmutableList($val);
   }
 
-  public static function createMutableList($val)
+  public static function mutableList($val)
   {
     return new MutableList($val);
   }
