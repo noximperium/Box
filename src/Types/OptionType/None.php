879d9bdc1c58ab8c166a2d09f0e0790dde68dace
Kaka <kakalition@gmail.com>
2022-11-19 17:46:51 +0700

    Modify Options

--- src/Types/OptionType/None.php
+++ src/Types/OptionType/None.php
@@ -4,4 +4,75 @@ namespace NoxImperium\Box\Types\OptionType;
 
 class None extends OptionType
 {
+  public function collect($pairs)
+  {
+  }
+
+  public function contains($element)
+  {
+  }
+
+  public function exists($predicate)
+  {
+  }
+
+  public function filter($predicate)
+  {
+  }
+
+  public function filterNot($predicate)
+  {
+  }
+
+  public function flatMap($function)
+  {
+  }
+
+  public function flatten()
+  {
+  }
+
+  public function fold($function)
+  {
+  }
+
+  public function forall($predicate)
+  {
+  }
+
+  public function foreach($function)
+  {
+  }
+
+  public function getOrElse($default)
+  {
+  }
+
+  public function isDefined()
+  {
+  }
+
+  public function isEmpty()
+  {
+  }
+
+  public function map($function)
+  {
+  }
+
+  public function orElse()
+  {
+  }
+
+  public function orNull()
+  {
+  }
+
+  public function toLeft($right)
+  {
+  }
+
+  public function toRight($left)
+  {
+  }
 }
--- src/Types/OptionType/Some.php
+++ src/Types/OptionType/Some.php
@@ -6,4 +6,75 @@ use NoxImperium\Box\Types\OptionType\OptionType;
 
 class Some extends OptionType
 {
+  public function collect($pairs)
+  {
+  }
+
+  public function contains($element)
+  {
+  }
+
+  public function exists($predicate)
+  {
+  }
+
+  public function filter($predicate)
+  {
+  }
+
+  public function filterNot($predicate)
+  {
+  }
+
+  public function flatMap($function)
+  {
+  }
+
+  public function flatten()
+  {
+  }
+
+  public function fold($function)
+  {
+  }
+
+  public function forall($predicate)
+  {
+  }
+
+  public function foreach($function)
+  {
+  }
+
+  public function getOrElse($default)
+  {
+  }
+
+  public function isDefined()
+  {
+  }
+
+  public function isEmpty()
+  {
+  }
+
+  public function map($function)
+  {
+  }
+
+  public function orElse()
+  {
+  }
+
+  public function orNull()
+  {
+  }
+
+  public function toLeft($right)
+  {
+  }
+
+  public function toRight($left)
+  {
+  }
 }
